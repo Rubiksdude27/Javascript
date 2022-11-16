422cfcd4bfbd83f7476dd7f5e9d9ee789455caf5
Rubiksdude27 <100394387+Rubiksdude27@users.noreply.github.com>
2022-11-16 14:51:13 -0600

    Create simple_game.js

---
+++ simple_game.js
@@ -0,0 +1,12 @@
+javascript:
+alert("wake up");
+alert("Hello, you here");
+alert("okay your awake");
+var play = confirm("you want to play this game?");
+if(play == true) {
+x = "okay was clicked";
+}
+else {
+x = "cancel was clicked";
+}
+alert(x);
