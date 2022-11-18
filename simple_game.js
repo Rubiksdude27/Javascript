59a5c9e0444524b8db6630e5388c953c89becfd6
Rubiksdude27 <100394387+Rubiksdude27@users.noreply.github.com>
2022-11-18 07:38:46 -0600

    Update simple_game.js

--- simple_game.js
+++ simple_game.js
@@ -11,4 +11,21 @@ x = "cancel was clicked";
 }
 alert(x);
 if(x == "okay was clicked") {
-prompt("What is the answer to this riddle, what runs around your fence but doesn't move");
+var y = prompt("What is the answer to this riddle, what runs around your yard but doesn't move?");
+if(y == "a fence"){
+alert("correct");
+var level2 = true
+}
+else{
+level2 = false
+}
+if(level2 == true){
+  var z = prompt("What is tall when its young and short when its old?");
+  if(z == "a candle"){
+  alert("correct");
+  var level3 = true
+  }
+  else{
+  level3 = false
+  }
+}
