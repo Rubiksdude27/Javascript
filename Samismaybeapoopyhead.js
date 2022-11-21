02146782e38f1895218568071e1d149c3b53d7ac
Rubiksdude27 <100394387+Rubiksdude27@users.noreply.github.com>
2022-11-21 11:54:16 -0600

    Create Samismaybeapoopyhead.js

---
+++ Samismaybeapoopyhead.js
@@ -0,0 +1,8 @@
+javascript:
+var x = confirm("Is sam a poopy head");
+if(x == true){
+alert("Sam is a poopy head, according to you")
+}
+else {
+alert("Sam is not a poopy head, according to you")
+}
